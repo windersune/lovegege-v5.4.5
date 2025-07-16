dcf0ad10903c6d0fd3348b2c8b27d7fbf7cb2a33
yangyang chen <154287759+windersune@users.noreply.github.com>
2025-07-16 17:36:49 +0800

    Update assistantStore.js

--- src/stores/assistantStore.js
+++ src/stores/assistantStore.js
@@ -17,7 +17,7 @@ export const useAssistantStore = defineStore('assistant', {
         description: '出身于GPT，娇弱身姿压制GPT阿布。',
         avatar: '/images/7.12-2.jpg',
         available: true,
-        supportsImage: false
+        supportsImage: true
       },
       {
         id: '3',
